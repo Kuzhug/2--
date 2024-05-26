f6f7f036a0bdbf71d87687f2cbe29bf8fb2bfb85
Kuzhug <alantos@internet.ru>
2024-05-26 12:43:10 +0700

    Add files via upload

---
+++ 4-ex/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пример использования if для возраста
+Console.WriteLine("Сколько вам лет?");
+int age = Convert.ToInt32(Console.ReadLine()); ;
+if (age < 7)
+{
+    Console.WriteLine("Иди в детский сад");
+}
+else if (age >= 7 && age < 18)
+{
+    Console.WriteLine("Иди в школу");
+}
+else if (age >= 18 && age < 23)
+{
+    Console.WriteLine("Иди в университет");
+}
+else if (age >= 23 && age < 60)
+{
+    Console.WriteLine("Иди на работу");
+}
+else
+{
+    Console.WriteLine("На пенсию пора");
+}
+
+// Система оценивания школьников
+Console.WriteLine("Какой балл?");
+int x = Convert.ToInt32(Console.ReadLine()); ;
+switch (x)
+{
+    case 12:
+        Console.WriteLine("Отлично!");
+        break;
+    case 9:
+    case 10:
+        Console.WriteLine("Хорошо!");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Удовлетворительно");
+        break;
+    default:
+        Console.WriteLine("Неудовлетворительно");
+        break;
+}
+
